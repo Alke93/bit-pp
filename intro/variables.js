cc93ce608521dc5fdcaadc17e130431a04aa3160
Nikola Alic <nikola@spesana.com>
2022-10-08 12:37:25 +0200

    Initial

---
+++ intro/variables.js
@@ -0,0 +1,112 @@
+// var a;
+// console.log(a);
+// var b = !a;
+// console.log(b);
+// a = 'Subota';
+// console.log(a);
+// b = !!a;
+// console.log(b);
+// console.log(true && "something");
+// console.log(1+'1');
+// console.log(true && undefined)
+// console.log(1 == '1');
+// console.log(1 === '1');
+// a = 1; 
+// a++;
+// console.log(1 !== '1')
+// console.log(1 >= '1')
+// // foo;
+// typeof foo;
+// var n = 2;
+// var nType = typeof n;
+// console.log(nType);
+// console.log(typeof nType);
+// console.log(typeof n == typeof a)
+// var octalNumber = 0377;
+// console.log(typeof octalNumber);
+// console.log(octalNumber);
+// var hexNumber = 0xAB;
+// console.log(typeof hexNumber);
+// console.log(hexNumber);
+// console.log(31e+2);
+// console.log(31e-2);
+// var a = 10 * 'a';
+// console.log(a);
+// var b = 1 + a;
+// console.log(b);
+// console.log(isNaN(b));
+// var s1 = 'web';
+// var s2 = "site";
+// var s3 = s1 + s2;
+// console.log(s3);
+// var error = "I don't know"
+// console.log(error);
+var s = '1s';
+// console.log(s++);
+// console.log(typeof -Infinity)
+// console.log(false == "")
+// var a = '';
+// if (a) {
+
+// }
+// console.log(typeof "2E+2")
+// a = 3e+3; a++;
+// console.log(a)
+
+// var check = s == '1s';
+// if (check) {
+//     console.log(s);
+// }
+
+// console.log(s == '1s')
+
+// if (s == '1s') {
+
+// }
+
+// var result = '', a = 0, b = 1;
+// if (a > 2 || a < -2) {
+//     result = 'a is not between -2 and 2';
+// } else if (a === 0 && b === 0) {
+//     result = 'both a and b are zeros';
+// } else if (a === b) {
+//     result = 'a and b are equal';
+// } else {
+//     result = 'I give up';
+// }
+
+// console.log(result);
+// x = 0;
+// if (x) {
+//     console.log('test');
+// }
+
+// var y = 1;
+// if (typeof y == 'undefined') {
+//     console.log('Does not exist')
+// }
+
+// typeof y == 'undefined' ? console.log('Does not exist') : console.log('It\'s alive');
+var d= 1, f = 2;
+if (d > f) {
+    console.log('D is Greater than F')
+} else if (f > d) {
+    console.log('F is Greater than D');
+} 
+else {
+    console.log('They are equal');
+}
+
+console.log( d % 2);
+if (d % 2 == 0) {
+    console.log(d + ' is divisible by 2')
+} else {
+    console.log(d + ' is not divisible by 2')
+}
+
+d = 12;
+if (d % 3 == 0 && d % 5 == 0) {
+    console.log(d + ' is divisible by 3 and 5')
+} else {
+    console.log(d + ' is not divisible by 3 and 5')
+}
